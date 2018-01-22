c60eb483940fe311c5f1d494099a1a7e3accc4b5
Ping-Ko Chiu <pingkoc@outlook.com>
2018-01-22 11:48:23 -0600

    created setup for pypi packages

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='cutnorm',
+    version='0.1.1',
+    description=
+    'Cutnorm approximation via Gaussian Rounding and Optimization with Orthogonality Constraints',
+    url='https://github.com/pingkoc/cutnorm',
+    author='Ping-Ko Chiu',
+    author_email='pchiu5@illinois.edu',
+    license='MIT',
+    packages=['cutnorm'],
+    zip_safe=False)
