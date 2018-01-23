a13632642ed631f2585d1784b05e4eea80a21824
Ping-Ko Chiu <pingkoc@outlook.com>
2018-01-23 10:04:34 -0600

    added requirements

--- setup.py
+++ setup.py
@@ -2,12 +2,24 @@ from setuptools import setup
 
 setup(
     name='cutnorm',
-    version='0.1.1',
+    version='0.1.2',
     description=
     'Cutnorm approximation via Gaussian Rounding and Optimization with Orthogonality Constraints',
     url='https://github.com/pingkoc/cutnorm',
     author='Ping-Ko Chiu',
     author_email='pchiu5@illinois.edu',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Scientific/Engineering :: Information Analysis',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+    ],
     license='MIT',
     packages=['cutnorm'],
+    install_requires=['numpy'],
+    python_requires='>=3',
     zip_safe=False)
