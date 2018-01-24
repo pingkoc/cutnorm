62bcad08df35fcd5abf80f7faac554ffee7b54fc
Ping-Ko Chiu <pingkoc@outlook.com>
2018-01-24 10:29:58 -0600

    added readme reference to the package setup

--- setup.py
+++ setup.py
@@ -1,13 +1,17 @@
 from setuptools import setup
 
+
+def readme():
+    with open('README.rst') as f:
+        return f.read()
+
+
 setup(
     name='cutnorm',
     version='0.1.2',
     description=
     'Cutnorm approximation via Gaussian Rounding and Optimization with Orthogonality Constraints',
-    url='https://github.com/pingkoc/cutnorm',
-    author='Ping-Ko Chiu',
-    author_email='pchiu5@illinois.edu',
+    long_description=readme(),
     classifiers=[
         'Development Status :: 3 - Alpha',
         'Intended Audience :: Developers',
@@ -18,8 +22,12 @@ setup(
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
     ],
+    url='https://github.com/pingkoc/cutnorm',
+    author='Ping-Ko Chiu',
+    author_email='pchiu5@illinois.edu',
     license='MIT',
     packages=['cutnorm'],
     install_requires=['numpy'],
     python_requires='>=3',
+    include_package_data=True,
     zip_safe=False)
